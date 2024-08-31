12f8390cb33cd0cc57587155ab05082198083f5f
kxujanazarov1c_ <132781171+kxujanazarov1c@users.noreply.github.com>
2024-08-31 16:22:48 +0500

    finished project

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let burger = document.querySelector(".nav_burger");
 let burger_menu = document.querySelector(".burger_menu");
 let exit_box = document.querySelector(".exit_box");
-let gmail = document.querySelector(".gmail_input")
+
 burger.addEventListener("click", () => {
   burger_menu.classList.add("burger_none");
 });
@@ -10,7 +10,5 @@ exit_box.addEventListener("click", () => {
   burger_menu.classList.remove("burger_none");
 });
 
-gmail.addEventListener("click" , ()=>{
-  console.log("hello");
-})
+
 
